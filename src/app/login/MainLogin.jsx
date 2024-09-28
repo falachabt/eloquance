716f6d183f7ec5cf5c293c9a56bbdc4ce10618a4
falachabt <bennytenezeu@gmail.com>
2024-09-28 15:15:02 +0200

    gg

--- src/app/login/MainLogin.jsx
+++ src/app/login/MainLogin.jsx
@@ -77,7 +77,7 @@ export const LoginPage = () => {
   };
 
   return (
-    <div className="min-h-screen bg-gray-100 flex flex-col sm:justify-center py-6 sm:py-12 px-4 sm:px-6 lg:px-8">
+    <div className="h-screen bg-gray-100 flex flex-col sm:justify-center py-6 sm:py-12 px-4 sm:px-6 lg:px-8">
       <div className="sm:mx-auto sm:w-full sm:max-w-md">
         <Link href={"/"}>
           <Image
